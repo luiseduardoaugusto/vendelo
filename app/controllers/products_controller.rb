15cb05e85aa8266640b10d8f4b5bd8bf72c66fae
luisaugusto <luis.augusto.u@gmail.com>
2023-02-16 22:56:41 -0500

    primer commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,45 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product, notice: 'Product was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
+
+
+end
